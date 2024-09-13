c91faf9426673265003a27810d18239332270b19
AndyFranco123 <af110636@gmail.com>
2024-09-13 02:54:04 -0300

    Tratando de subir la pagina1

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -56,7 +56,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'djangocrud.urls'
@@ -133,7 +136,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'tasks.Usuario'
+# Configura STATIC_ROOT para la carpeta donde Django almacenará los archivos estáticos tras ejecutar collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Esto asegura que Django sirva los archivos estáticos correctamente
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
